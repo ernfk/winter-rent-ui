2821349ce3ea7f5c810787845b6fa041d226dd12
ernfk <ernest.filip.kret@gmail.com>
2019-05-15 20:03:44 +0200

    Delete: Unnecessary code

--- src/reducers/items.js
+++ src/reducers/items.js
@@ -4,37 +4,21 @@ import createReducer from '../utils/create-reducer';
 export const initialState = {
   itemTypes: [],
   itemPropertyDefinitions: [],
-  snackbarInfoType: '',
-  snackbarMessage: '',
-  snackbarOpenStatus: false,
   items: [],
   itemImage: {},
 };
 
-const setCloseSnackBar = state => ({
-  ...state, snackbarMessage: '', snackbarOpenStatus: false,
-});
-
 const setFetchedItemDefinitions = (state, action) => ({ ...state, itemPropertyDefinitions: action.itemPropertyDefinitions });
 
 const setFetchedItemTypes = (state, action) => ({ ...state, itemTypes: action.itemTypes });
-
-const setShowSnackBar = (state, action) => ({
-  ...state,
-  snackbarInfoType: action.typeMessage,
-  snackbarMessage: action.message,
-  snackbarOpenStatus: true,
-});
 
 const setFetchedItems = (state, action) => ({ ...state, items: action.items });
 
 const setItemImage = (state, action) => ({ ...state, itemImage: action.image });
 
 export const items = createReducer(initialState, {
-  [ItemsActionsTypes.CLOSE_SNACK_BAR]: setCloseSnackBar,
   [ItemsActionsTypes.FETCHED_ITEM_DEFINITIONS]: setFetchedItemDefinitions,
   [ItemsActionsTypes.FETCHED_ITEM_TYPES]: setFetchedItemTypes,
-  [ItemsActionsTypes.SHOW_SNACK_BAR]: setShowSnackBar,
   [ItemsActionsTypes.FETCHED_ITEMS]: setFetchedItems,
   [ItemsActionsTypes.FETCHED_ITEM_IMAGE]: setItemImage,
 });
