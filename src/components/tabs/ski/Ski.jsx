6149f868515b53b515c4e3f85c6889209d4a9f66
ernfk <ernest.filip.kret@gmail.com>
2020-02-28 17:08:10 +0100

    Add: Initial web sockets

--- src/components/tabs/ski/Ski.jsx
+++ src/components/tabs/ski/Ski.jsx
@@ -1,12 +1,47 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 import AppMenu from '../../overview/AppMenu';
 import Title from '../../commons/title/Title';
 
-const Ski = () => (
-  <div>
-    <AppMenu />
-    <Title title="SKI" />
-  </div>
-);
+class Ski extends React.Component {
+  constructor() {
+    super();
+  }
+
+    handleTestWebSockets =() => {
+      console.log('Testing WebSocket');
+      const ws = new WebSocket('ws://localhost:6060/resources/app/chat.addUser',
+        ['Authorization', window.localStorage.getItem('accessToken')]);
+
+      ws.onopen = () => {
+        // on connecting, do nothing but log it to the console
+        console.log('Connected');
+      };
+    };
+
+    render() {
+      return (
+        <div>
+          <AppMenu />
+          <Title title="SKI" />
+          <Button onClick={this.handleTestWebSockets}>
+            {' '}
+            {'WS'}
+            {' '}
+          </Button>
+          <Button onClick={this.handleTestWebSockets2}>
+            {' '}
+            {'WS'}
+            {' '}
+          </Button>
+          <Button onClick={this.handleTestWebSockets3}>
+            {' '}
+            {'WS'}
+            {' '}
+          </Button>
+        </div>
+      );
+    }
+}
 
 export default Ski;
